70121cfd21efe5698338ef81ad22307de317827a
Vitor José <vitorjsls30@gmail.com>
2017-11-04 16:40:53 -0200

    react-5 - Adding Unique Keys example and adjusting the NumberList component

--- client/lists-and-keys/rendering-multiple/index.js
+++ client/lists-and-keys/rendering-multiple/index.js
@@ -8,13 +8,13 @@ function ListItem(props) {
 function NumberList(props) {
     const numbers = props.numbers;
 
-    const listItems = numbers.map((number) => 
-        <ListItem key={number.toString()}
-                value={number} />
-    );
-
-    return(
-        <ul>{listItems}</ul>
+    return (
+        <ul>
+        {numbers.map((number) => 
+            <ListItem key={number.toString()}
+                    value={number} />
+        )}
+        </ul>
     );
 } 
 
