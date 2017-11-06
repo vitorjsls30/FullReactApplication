1427902b937964a2a8998d4965fab8dc39a49273
Vitor José <vitorjsls30@gmail.com>
2017-11-05 23:05:59 -0200

    react-6 - Adding initial structure for the select tag example

---
+++ client/forms/select-tag/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FlavorForm extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+ReactDOM.render(
+    <FlavorForm />,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports  = {
-    entry: './client/forms/textarea-tag/index.js',
+    entry: './client/forms/select-tag/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
